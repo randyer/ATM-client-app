97351efa031fddec4aac1fe592fa0b6868b038fc
randyer <rbdyer3@gmail.com>
2024-11-13 12:20:43 -0500

    app demo created

--- src/App.js
+++ src/App.js
@@ -27,16 +27,23 @@ import "@aws-amplify/ui-react/styles.css";
 import { Button, Dropdown } from "react-bootstrap";
 
 function App() {
-  const [clients, setClients] = useState([]);
+  let clientTesting = require("./helper/clientList.json");
+  const [clients, setClients] = useState(clientTesting);
   const [newClient, setNewClient] = useState({});
   const [refreshClicked, setRefreshClicked] = useState(false);
   const [sortMethod, setSortMethod] = useState("alphabetical");
   const [showModal, setShowModal] = useState(false);
+  const [showDisclaimer, setShowDisclaimer] = useState(true);
   const handleModalClose = () => setShowModal(false);
   const handleModalShow = () => setShowModal(true);
 
+  const handleDisclaimerClose = () => {
+    setShowDisclaimer(false);
+  };
+
   useEffect(() => {
-    fetchClients(setClients);
+    // fetchClients(setClients);
+    console.log("clients: ", clients);
   }, []);
 
   useEffect(() => {
@@ -153,166 +160,217 @@ function App() {
 
   return (
     <div className="App flex flex-col">
-      <Authenticator hideSignUp={true}>
-        {({ signOut }) => (
-          <Router basename="">
-            <ScrollToTop />
+      {/* <Authenticator hideSignUp={true}> */}
+      {/* {({ signOut }) => ( */}
+      <Router basename="">
+        <ScrollToTop />
 
-            <Routes>
-              <Route
-                path="/"
-                element={
-                  <>
-                    <div className="sticky">
-                      {/* <div> */}
-                      <header className="App-header">
-                        <button
-                          onClick={() => {
-                            setRefreshClicked(true);
-                            fetchClients(setClients);
-                            setTimeout(() => setRefreshClicked(false), 300); // Reset after 300ms
-                          }}
-                          className={refreshClicked ? "button-clicked" : ""}
-                        >
-                          <h1 className="flex items-center">
-                            Clients <Refresh className="svg-icon" />
-                          </h1>
-                        </button>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                {showDisclaimer && (
+                  <div className="disclaimer-popup">
+                    <div className="disclaimer-popup-content">
+                      <h1>Disclaimer</h1>
+                      <p>
+                        This is a demo. Please note that all of the data
+                        presented here is dummy data for demonstration purposes
+                        only.
+                      </p>
+                      <p>
+                        This web app is designed for mobile devices. Its primary
+                        purpose is to help massage therapists and businesses
+                        manage their clients effectively. The app includes four
+                        main tabs:
+                      </p>
+                      <ul>
+                        <li>
+                          <strong>Active:</strong> Clients currently being seen.
+                        </li>
+                        <li>
+                          <strong>Waitlist:</strong> Clients waiting to be seen.
+                        </li>
+                        <li>
+                          <strong>Re-book:</strong> Clients who need to be
+                          re-booked for future appointments.
+                        </li>
+                        <li>
+                          <strong>Archive:</strong> Clients whose profiles are
+                          archived.
+                        </li>
+                      </ul>
+                      <p>By clicking on a client, you can:</p>
+                      <ul>
+                        <li>Favorite the client.</li>
+                        <li>
+                          Toggle that there are important notes about the
+                          client.
+                        </li>
+                        <li>
+                          Change the client's status between the four tabs.
+                        </li>
+                        <li>
+                          Keep track of general notes and appointment-specific
+                          notes like symptoms, assessment, and plan.
+                        </li>
+                      </ul>
+                      <p>Adding a client puts them in the waitlist tab.</p>
+                      <p>
+                        To reset the client list, click the refresh button on
+                        the home screen in the top left.
+                      </p>
 
-                        <AddButton
-                          onClick={handleModalShow}
-                          className="svg-icon"
-                        ></AddButton>
-                        <AddClientModal
-                          showModal={showModal}
-                          handleModalClose={handleModalClose}
-                          handleSubmitClient={addClient} // Pass addClient as a prop to submit the new client
-                        />
-                        <SignOut
-                          onClick={signOut}
-                          className="svg-icon"
-                        ></SignOut>
-                      </header>
-                      <div className="tabs">
-                        <button
-                          className={`tab ${
-                            activeTab === "today" ? "active" : ""
-                          }`}
-                          onClick={() => setActiveTab("today")}
-                        >
-                          Today
-                        </button>
-                        <button
-                          className={`tab ${
-                            activeTab === "active" ? "active" : ""
-                          }`}
-                          onClick={() => setActiveTab("active")}
-                        >
-                          Active
-                        </button>
-                        <button
-                          className={`tab ${
-                            activeTab === "waitlist" ? "active" : ""
-                          }`}
-                          onClick={() => setActiveTab("waitlist")}
-                        >
-                          Waitlist
-                        </button>
-                        <button
-                          className={`tab ${
-                            activeTab === "re-book" ? "active" : ""
-                          }`}
-                          onClick={() => setActiveTab("re-book")}
-                        >
-                          Book
-                        </button>
-                        <button
-                          className={`tab ${
-                            activeTab === "archive" ? "active" : ""
-                          }`}
-                          onClick={() => setActiveTab("archive")}
-                        >
-                          Archive
-                        </button>
-                      </div>
-                      <div className="search-and-sort">
-                        <input
-                          type="text"
-                          placeholder="Search"
-                          value={search}
-                          onChange={(e) => setSearch(e.target.value)}
-                          className="search-bar"
-                        />
-                        <Dropdown>
-                          <Dropdown.Toggle
-                            id="dropdown-basic"
-                            className="dropdown-button flex flex-row"
-                          >
-                            {sortMethod === "alphabetical" && (
-                              <SortAlpha className="svg-icon" />
-                            )}
-                            {sortMethod === "queue" && (
-                              <Upcoming className="svg-icon" />
-                            )}
-                            {sortMethod === "recentlyModified" && (
-                              <Modified className="svg-icon" />
-                            )}
-                          </Dropdown.Toggle>
-
-                          <Dropdown.Menu>
-                            <Dropdown.Item
-                              onClick={() => setSortMethod("alphabetical")}
-                              className={
-                                sortMethod === "alphabetical" ? "selected" : ""
-                              }
-                            >
-                              By Name
-                            </Dropdown.Item>
-                            <Dropdown.Item
-                              onClick={() => setSortMethod("queue")}
-                              className={
-                                sortMethod === "queue" ? "selected" : ""
-                              }
-                            >
-                              Queue
-                            </Dropdown.Item>
-                            <Dropdown.Item
-                              onClick={() => setSortMethod("recentlyModified")}
-                              className={
-                                sortMethod === "recentlyModified"
-                                  ? "selected"
-                                  : ""
-                              }
-                            >
-                              Recently modified
-                            </Dropdown.Item>
-                          </Dropdown.Menu>
-                        </Dropdown>
-                      </div>
+                      <button onClick={handleDisclaimerClose}>
+                        I Understand
+                      </button>
                     </div>
-                    <ClientList
-                      clients={filteredClients}
-                      getInitials={getInitials}
-                      waitlist={activeTab === "waitlist"}
-                      setClients={setClients}
-                      sortMethod={sortMethod}
-                      setSortMethod={setSortMethod}
+                  </div>
+                )}
+                <div className="sticky">
+                  {/* <div> */}
+                  <header className="App-header">
+                    <button
+                      onClick={() => {
+                        setRefreshClicked(true);
+                        setClients(clientTesting);
+                        setTimeout(() => setRefreshClicked(false), 300); // Reset after 300ms
+                      }}
+                      className={refreshClicked ? "button-clicked" : ""}
+                    >
+                      <h1 className="flex items-center">
+                        Clients <Refresh className="svg-icon" />
+                      </h1>
+                    </button>
+
+                    <AddButton
+                      onClick={handleModalShow}
+                      className="svg-icon"
+                    ></AddButton>
+                    <AddClientModal
+                      showModal={showModal}
+                      handleModalClose={handleModalClose}
+                      handleSubmitClient={addClient} // Pass addClient as a prop to submit the new client
                     />
-                  </>
-                }
-              />
-              <Route
-                path="/client/:id"
-                element={
-                  <ClientInfo clients={clients} setClients={setClients} />
-                }
-              />
-            </Routes>
-          </Router>
-        )}
-      </Authenticator>
+                    <SignOut
+                      // onClick={signOut}
+                      className="svg-icon"
+                    ></SignOut>
+                  </header>
+                  <div className="tabs">
+                    <button
+                      className={`tab ${activeTab === "today" ? "active" : ""}`}
+                      onClick={() => setActiveTab("today")}
+                    >
+                      Today
+                    </button>
+                    <button
+                      className={`tab ${
+                        activeTab === "active" ? "active" : ""
+                      }`}
+                      onClick={() => setActiveTab("active")}
+                    >
+                      Active
+                    </button>
+                    <button
+                      className={`tab ${
+                        activeTab === "waitlist" ? "active" : ""
+                      }`}
+                      onClick={() => setActiveTab("waitlist")}
+                    >
+                      Waitlist
+                    </button>
+                    <button
+                      className={`tab ${
+                        activeTab === "re-book" ? "active" : ""
+                      }`}
+                      onClick={() => setActiveTab("re-book")}
+                    >
+                      Book
+                    </button>
+                    <button
+                      className={`tab ${
+                        activeTab === "archive" ? "active" : ""
+                      }`}
+                      onClick={() => setActiveTab("archive")}
+                    >
+                      Archive
+                    </button>
+                  </div>
+                  <div className="search-and-sort">
+                    <input
+                      type="text"
+                      placeholder="Search"
+                      value={search}
+                      onChange={(e) => setSearch(e.target.value)}
+                      className="search-bar"
+                    />
+                    <Dropdown>
+                      <Dropdown.Toggle
+                        id="dropdown-basic"
+                        className="dropdown-button flex flex-row"
+                      >
+                        {sortMethod === "alphabetical" && (
+                          <SortAlpha className="svg-icon" />
+                        )}
+                        {sortMethod === "queue" && (
+                          <Upcoming className="svg-icon" />
+                        )}
+                        {sortMethod === "recentlyModified" && (
+                          <Modified className="svg-icon" />
+                        )}
+                      </Dropdown.Toggle>
+
+                      <Dropdown.Menu>
+                        <Dropdown.Item
+                          onClick={() => setSortMethod("alphabetical")}
+                          className={
+                            sortMethod === "alphabetical" ? "selected" : ""
+                          }
+                        >
+                          By Name
+                        </Dropdown.Item>
+                        <Dropdown.Item
+                          onClick={() => setSortMethod("queue")}
+                          className={sortMethod === "queue" ? "selected" : ""}
+                        >
+                          Queue
+                        </Dropdown.Item>
+                        <Dropdown.Item
+                          onClick={() => setSortMethod("recentlyModified")}
+                          className={
+                            sortMethod === "recentlyModified" ? "selected" : ""
+                          }
+                        >
+                          Recently modified
+                        </Dropdown.Item>
+                      </Dropdown.Menu>
+                    </Dropdown>
+                  </div>
+                </div>
+                <ClientList
+                  clients={filteredClients}
+                  getInitials={getInitials}
+                  waitlist={activeTab === "waitlist"}
+                  setClients={setClients}
+                  sortMethod={sortMethod}
+                  setSortMethod={setSortMethod}
+                />
+              </>
+            }
+          />
+          <Route
+            path="/client/:id"
+            element={<ClientInfo clients={clients} setClients={setClients} />}
+          />
+        </Routes>
+      </Router>
+      {/* )} */}
+      {/* </Authenticator> */}
     </div>
   );
 }
 
-export default withAuthenticator(App);
+// export default withAuthenticator(App);
+export default App;
